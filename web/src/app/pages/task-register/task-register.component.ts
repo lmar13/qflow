d13f22fdf1043a1a6bb5590a1d1bba98a9710144
lukasz.marciniak <lukasz.marciniak@thomsonreuters.com>
2019-02-11 14:31:16 +0100

    add task register

---
+++ web/src/app/pages/task-register/task-register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-task-register',
+  templateUrl: './task-register.component.html',
+  styleUrls: ['./task-register.component.scss']
+})
+export class TaskRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
