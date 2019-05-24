bf562fed7d424e47fee5cb7a34cc5e52f8391b89
lukasz.marciniak <lukasz.marciniak@thomsonreuters.com>
2019-05-24 10:22:22 +0200

    environment production

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -2,5 +2,7 @@ export const environment = {
   production: true,
   // appToken: 'Lm2Bj4Z*Fy6U$2u?',
   // apiBaseUri: 'http://localhost:8080/rap/api'
-  apiBaseUri: "http://10.7.102.162:3000"
+  // apiBaseUri: "http://10.7.102.162:3000"
+  // apiBaseUri: "http://localhost:3000"
+  apiBaseUri: "http://10.218.75.164:3000"
 };
